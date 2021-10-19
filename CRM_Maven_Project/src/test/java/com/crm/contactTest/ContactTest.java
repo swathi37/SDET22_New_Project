f193a1ea9460e6f96550d7ecdf1a9823d3e15880
USER <USER@LAPTOP-NBA1G7PD>
2021-10-19 13:39:58 +0530

    Commit

---
+++ CRM_Maven_Project/src/test/java/com/crm/contactTest/ContactTest.java
@@ -0,0 +1,20 @@
+package com.crm.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("execute createContactTest");
+	}
+	
+	@Test
+	public void editContactTest()
+	{
+		System.out.println("execute editContactTest");
+		String username = System.getProperty("user");
+		 System.out.println("======="+username+"========");
+	}
+
+}
